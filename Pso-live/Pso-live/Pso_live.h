44f77347467cb5b4c605a4a08efee20592faf183
Shairjeel Ahmed <>
2020-01-12 23:51:16 +0500

    inilialize project

---
+++ Pso-live/Pso-live/Pso_live.h
@@ -0,0 +1,19 @@
+//
+//  Pso_live.h
+//  Pso-live
+//
+//  Created by Shairjeel Ahmed on 12/01/2020.
+//  Copyright © 2020 Shairjeel Ahmed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Pso_live.
+FOUNDATION_EXPORT double Pso_liveVersionNumber;
+
+//! Project version string for Pso_live.
+FOUNDATION_EXPORT const unsigned char Pso_liveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pso_live/PublicHeader.h>
+
+
