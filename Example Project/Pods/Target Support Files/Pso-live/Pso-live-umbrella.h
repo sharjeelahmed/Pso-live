a3de5eccd8dd0108e77b3618993d8e4614878f9e
sharjeelahmed <sharjeelahmed92@hotmail.com>
2020-01-21 16:46:44 +0500

    remove launch StoryBoard

---
+++ Example Project/Pods/Target Support Files/Pso-live/Pso-live-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "APPinView.h"
+#import "APPinViewController.h"
+#import "MPGTextField.h"
+#import "Pso_live.h"
+#import "Voucher365-Bridging-Header.h"
+
+FOUNDATION_EXPORT double Pso_liveVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pso_liveVersionString[];
+
